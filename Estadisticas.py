b198307420651160e9f5740106297fcd306a0027
captuayo <$4b1t84*>
2018-03-16 21:14:14 -0500

    Iteración 1 - Ciclo 4 - Se crea test para obtener el número de elementos de un string de N números (verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -1,8 +1,9 @@
 __author__ = ""
 
+
 class Estadisticas:
     def calcular(self, cadenaInicial):
-        if cadenaInicial == "": return 0
-        elif len(cadenaInicial) == 1: return 1
-        elif len(cadenaInicial.split(',')) == 2: return 2
-
+        if cadenaInicial == "":
+            return 0
+        else:
+            return len(cadenaInicial.split(','))

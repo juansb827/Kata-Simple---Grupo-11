25d1c72f6cda1a2cb4e3c598d1e5d95f709c254f
Juan Bedoya <juansb827@gmail.com>
2018-03-16 21:55:37 -0500

    Iteración 3 - Ciclo 1 - Se crea test para obtener el número de elementos, el mínimo y el máximo de un string vacio (verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -7,9 +7,5 @@ class Estadisticas:
         if cadenaInicial == "":
             response.append(0)
             response.append(None)
+            response.append(None)
             return response
-        else:
-            array = cadenaInicial.split(',')
-            response.append(len(array))
-            response.append(int(min(array)))
-        return response

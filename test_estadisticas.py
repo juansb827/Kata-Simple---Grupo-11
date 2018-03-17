e9c7a03fbd47b89a9c5085aaf0def557b712c47f
captuayo <$4b1t84*>
2018-03-16 22:00:47 -0500

    Iteración 3 - Ciclo 3 - Se crea test para obtener el número de elementos, el mínimo y el máximo de un string de 2 números (rojo)

--- test_estadisticas.py
+++ test_estadisticas.py
@@ -11,3 +11,7 @@ class EstadisticasTest(TestCase):
     def test_num_elementos_cadena_uno(self):
         response = [1, 1, 1]
         self.assertEqual(Estadisticas().calcular("1"), response, "Numero de elementos Cadena 1 elemento")
+
+    def test_num_elementos_cadena_uno(self):
+        response = [2, 1, 1]
+        self.assertEqual(Estadisticas().calcular("10,1"), response, "Numero de elementos Cadena 2 elemento")

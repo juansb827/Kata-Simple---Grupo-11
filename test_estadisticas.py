e593c6a3dc8731f8ac80d1e4a7588bb25945d93b
Juan Bedoya <juansb827@gmail.com>
2018-03-16 21:37:36 -0500

    Iteración 2 - Ciclo 2 - Se crea test para obtener el número de elementos y el mínimo de un string de 1 número (rojo)

--- test_estadisticas.py
+++ test_estadisticas.py
@@ -8,3 +8,8 @@ class EstadisticasTest(TestCase):
         response = [0, None]
         self.assertEqual(Estadisticas().calcular(""), response, "Numero de elementos Cadena vacia")
 
+    def test_num_elementos_uno(self):
+        response = [1, 1]
+        self.assertEqual(Estadisticas().calcular("1"), response, "Numero de elementos cadena de 1 elemento")
+
+

292464589f7a3e0fedf2ec31b3b80672b01b6da5
captuayo <$4b1t84*>
2018-03-16 20:42:30 -0500

    Iteración 1 - Ciclo 2 - Se crea test para obtener el número de elementos de un string de 1 número (rojo)

--- test_estadisticas.py
+++ test_estadisticas.py
@@ -6,3 +6,6 @@ class EstadisticasTest(TestCase):
 
     def test_num_elementos_cadena_vacia(self):
         self.assertEqual(Estadisticas().calcular(""), 0, "Numero de elementos Cadena vacia")
+
+    def test_num_elementos_uno(self):
+        self.assertEqual(Estadisticas().calcular("1"), 1, "Numero de elementos cadena de 1 elemento")

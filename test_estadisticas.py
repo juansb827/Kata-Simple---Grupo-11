c0082fd4a1488eb0a3e9c431674b918cd348a93b
Juan Bedoya <juansb827@gmail.com>
2018-03-16 22:18:04 -0500

    Iteración 4 - Ciclo 2 - Se crea test para obtener el número de elementos, el mínimo, el máximo y el promedio de un string de 1 número (rojo)

--- test_estadisticas.py
+++ test_estadisticas.py
@@ -10,7 +10,7 @@ class EstadisticasTest(TestCase):
         self.assertEqual(Estadisticas().calcular(""), response, "Numero de elementos Cadena vacia")
 
     def test_num_elementos_cadena_uno(self):
-        response = [1, 1, 1]
+        response = [1, 1, 1,1]
         self.assertEqual(Estadisticas().calcular("1"), response, "Numero de elementos Cadena 1 elemento")
 
     def test_num_elementos_cadena_dos(self):
